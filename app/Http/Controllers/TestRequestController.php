823f467f35826e657a36abe4065714dc943c4847
hannykoko <bcmm.intern01@brycenmyanmar.com.mm>
2022-05-27 16:50:07 +0630

    Image resize added

--- app/Http/Controllers/TestRequestController.php
+++ app/Http/Controllers/TestRequestController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Intervention\Image\Facades\Image;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -70,28 +70,45 @@ class TestRequestController extends Controller
 
     
     public function fileUpload(Request $request){
-        #1 file upload
-        $file = $request->file('file');
-        Log::info($file);
-        $name = $request->file->getClientOriginalName();
-        Log::info($name);
-        $content =file_get_contents($request->file);
-        Log::info($content);
-        $path = $request->file('file')->storeAs('public',$name);
+        // #1 file upload
+        // $file = $request->file('file');
+        // //Log::info($file);
+        // $name = $request->file->getClientOriginalName();
+        // //Log::info($name);
+        // $content =file_get_contents($request->file);
+        // //Log::info($content);
+        
+        // $upload = $request->file('file')->storeAs('public',$name);
 
-        #2 file download
-        if($path){
-            Log::info($path);
-            // $tmpFileName = 'kites.png';
-            $path = storage_path().'/app/public/'.$name;
-            $headers = [
-                'Access-Control-Allow-Origin' => '*',
-                'Content-Description' => 'File Transfer',
-                'Content-Disposition' => 'attachment;filename=' . $name,
-                'Access-Control-Expose-Headers' => 'Content-Disposition,X-Suggested-Filename'
-            ];
-            return response()->download($path, $name, $headers);
-        }
+        #resize uploded image
+        $originalImage= $request->file('filename');
+        // echo 'File Size: '.$originalImage->getSize();
+        $height = Image::make($originalImage)->height();
+        $width = Image::make($originalImage)->width();
+        echo 'Img height:' .$height;
+        echo '<br>';
+        echo 'Img width:' .$width;
+        $thumbnailImage = Image::make($originalImage);
+        $thumbnailPath = storage_path().'/app/';
+        // $originalPath = public_path().'/images/';
+        // $thumbnailImage->save($originalPath.time().$originalImage->getClientOriginalName());
+        $thumbnailImage->resize(150,150);
+        $thumbnailImage->save($thumbnailPath.time().$originalImage->getClientOriginalName()); 
+
+        
+        // #2 file download
+        // if($upload){
+        //     //Log::info($path);
+        //     // $tmpFileName = 'kites.png';
+        //     $path = storage_path().'/app/public/'.$name;
+        //     $headers = [
+        //         'Access-Control-Allow-Origin' => '*',
+        //         'Content-Description' => 'File Transfer',
+        //         'Content-Disposition' => 'attachment;filename=' . $name,
+        //         'Access-Control-Expose-Headers' => 'Content-Disposition,X-Suggested-Filename'
+        //     ];
+        //     return response()->download($path, $name, $headers);
+        // }
     }
         
 }
--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        'Intervention\Image\ImageServiceProvider',
 
         /*
          * Application Service Providers...
@@ -226,7 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        
+        'Image' => 'Intervention\Image\Facades\Image',
     ],
 
 ];
